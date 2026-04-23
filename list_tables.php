4dd78e0fecc7918c10eadbdd6f5085672ffa7f4c
marcotrm <marcotrm@users.noreply.github.com>
2026-04-23 14:29:46 +0200

    chore: remove test scripts

--- list_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$tables = DB::select("SELECT name FROM sqlite_master WHERE type='table'");
-foreach($tables as $t) echo $t->name . "\n";
